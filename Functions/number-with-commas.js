04b7cf8bb0f4d63a6cd790b67361fafda2cf81cb
Chris Koh <chris.koh90@gmail.com>
2015-10-15 09:56:09 -0700

    Create number-with-commas.js

---
+++ Functions/number-with-commas.js
@@ -0,0 +1,10 @@
+/*-------------------------------------------------------------------------------------------------
+Function: numberWithCommas(x)
+Purpose:  print numbers with commas
+-------------------------------------------------------------------------------------------------*/
+function numberWithCommas(x){
+	
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
